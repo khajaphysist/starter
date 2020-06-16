562f6ecac8024980cb755d804affbac2288b5f69
Khaja Mohiddin Shaik <khajaphysist@gmail.com>
2020-06-17 00:45:23 +0900

    use react fast refresh

--- client/craco.config.js
+++ client/craco.config.js
@@ -1,4 +1,6 @@
 const CracoLessPlugin = require("craco-less");
+const { whenDev } = require('@craco/craco');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
 module.exports = {
   plugins: [
@@ -14,4 +16,36 @@ module.exports = {
       },
     },
   ],
+  webpack: {
+    configure: (webpackConfig) => {
+      whenDev(() => {
+        const babelLoaderRule = webpackConfig.module.rules
+          .find((x) => Array.isArray(x.oneOf))
+          .oneOf.find(
+            (x) =>
+              x.loader &&
+              x.loader.includes('babel-loader') &&
+              x.options &&
+              x.options.presets &&
+              x.options.presets.some((p) => p.includes('babel-preset-react-app'))
+          );
+
+        babelLoaderRule.options.plugins.push(require.resolve('react-refresh/babel'));
+      });
+
+      return webpackConfig;
+    },
+    plugins: [
+      ...whenDev(
+        () => [
+          new ReactRefreshWebpackPlugin({
+            // Proper setup for this would require the new unpublished version of CRA.
+            // We'll disable the integration for now.
+            overlay: false,
+          }),
+        ],
+        []
+      ),
+    ],
+  },
 };
